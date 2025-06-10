d60684917a0d2888f66c00792a2ef5d4bcdc3d38
ingediego94 <inge.diego94@gmail.com>
2025-06-09 21:36:49 -0500

    Se agregan 3 archivos

---
+++ 02_tipos_datos/tipos_datos.js
@@ -0,0 +1,22 @@
+
+let nombre = 'Carlos';              // Tipo string
+let edad = 57;                      //Tipo number
+let precio = 3.1416;                // Tipo number
+let esBooleano = false;             // Tipo Boolean
+let esNulo = null;                  // Null
+let lista = [99, 'Camilo', true];   // Tipo lista
+let producto = {tipo: 'Pantalon', cantidad: 27};    //Tipo objeto
+
+console.log("Hola" + nombre);
+console.log(edad);
+console.log(precio);
+console.log(esBooleano);
+console.log(esNulo);
+
+
+console.log(lista);
+console.log(lista[0]);      // Accede al 1er elemento de la lista
+
+
+console.log(producto);
+console.log(producto.cantidad);     //Accede al valor de la clave 'cantidad'
---
+++ 03_concatenacion/concatenacion.js
@@ -0,0 +1,12 @@
+
+let nombre = 'Diego';
+let edad = 29;
+
+// Concatenacion de variables
+console.log("Hola, soy " + nombre + " y tengo " + edad + " años.");
+
+// Con comilla invertida y ${}
+console.log(`Hola a todos. ${nombre} y mi edad es ${edad}.`);
+
+// Separado por comas.
+console.log("Hola, mi nombre es", nombre, "y tengo", edad, "años.");
