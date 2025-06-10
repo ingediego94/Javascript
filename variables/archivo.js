f2746f04336205439e80929884eb95a74cd00a8a
ingediego94 <inge.diego94@gmail.com>
2025-06-09 20:32:39 -0500

    first commit

---
+++ variables/archivo.js
@@ -0,0 +1,22 @@
+// Comentario de una linea
+
+/*Comentario
+de
+varias
+lineas */
+
+
+// var nombre = 'Camila';       Forma antigua de variables
+
+let nombre = 'Diego';
+let Nombre = 'Ofelia';
+let nombres = 'Camila y Arturo'
+
+// Escribe el contenido en el html con salto de linea. Obsoleto.
+document.writeln(Nombre);
+
+// Monstrar mensaje por consola navegador.
+console.log(nombre);
+
+//
+document.getElementById("Saludo").textContent = nombres;
