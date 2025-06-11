4be4b83eb73db2c60e5f88e9df73561ed824c7f9
ingediego94 <inge.diego94@gmail.com>
2025-06-10 21:03:47 -0500

    Se agregan 3 archivos

--- 01_variables/archivo.js
+++ 01_variables/archivo.js
@@ -20,3 +20,92 @@ console.log(nombre);
 
 //
 document.getElementById("Saludo").textContent = nombres;
+
+
+// We can declare multiple variables only with one 'let' at the beggining, 
+// but it these are separated by commas.
+let mother = 'Vanesa', father = 'Carlos', sister = 'Camilo', price=200;
+
+
+// We can do the same in many lines as we want as long as all of them are separated
+// by commas.
+let product1 = 'T-shirt',
+product2 = 'Jeans',
+product3 = 'Shoes';
+
+
+// We can't re-declare a variable with 'let' or 'const' it doesn't allowed, this is 
+// possible only with 'var'.
+let carName = 'BMW';
+// let carName;     // this shows an error
+
+// on the other hand, this is allowed, it hasn't errors.
+var carName1 = 'Volvo';
+var carName1;
+
+
+// Arithmetic
+// This is a sum:
+let y = 5 + 2 + 3;                  // equal = 10
+
+// This is a concatenation of text.
+let x = "John" + " " + "Doe";       //equal= John Doe
+
+// This is another concatenation inside a variable.
+let z = 2 + 3 + "5";                //equal= 235 (as a text)
+
+
+// const variables should be assigned when they are created:
+const pi = 3.1416324;
+// This is an error, because they are not in the same line:
+// const pi;
+// 3.1416324
+
+// When to use const: A new Array:  for A new Object, A new Function, A new RegExp
+
+
+/*
+Block Scope:
+Variables declared inside a { } block cannot be accessed from outside the block:
+
+{
+    let x = 2;
+}
+
+x can NOT be used here 
+
+
+
+Global Scope
+Variables declared with the var always have Global Scope.
+Variables declared with the var keyword can NOT have block scope.
+Variables declared with varinside a { } block can be accessed from outside the block:
+
+{
+    var x = 2;
+}
+
+
+x CAN be used here
+
+---------------------------------------------------
+
+Cannot be Redeclared
+
+With let you can not do this:
+
+let x = "John Doe";
+
+let x = 0;
+
+
+
+Variables defined with var can be redeclared.
+
+With var you can do this:
+
+var x = "John Doe";
+
+var x = 0;
+
+*/
---
+++ 04_operadores/operadores.js
@@ -0,0 +1,39 @@
+/*
+
+Arithmetic operators
++, -, *, **, /, %, ++ (increment), -- (decrement)
+
+
+
+Assignment Operators
+=, +=, -=, *=, /=, %=, **=
+
+
+
+Comparison Operators:
+Operator	Description
+==	        equal to
+===	        equal value and equal type
+!=	        not equal
+!==	        not equal value or not equal type
+>	        greater than
+<	        less than
+>=	        greater than or equal to
+<=	        less than or equal to
+?	        ternary operator
+
+
+Logical Operators:
+Operator	Description
+&&	        logical and
+||	        logical or
+!	        logical not
+
+
+
+Type Operators
+Operator	    Description
+typeof	        Returns the type of a variable
+instanceof	    Returns true if an object is an instance of an object type
+
+*/
