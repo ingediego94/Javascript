04da13b01744b326666d99cceb39b324c49fcde2
ingediego94 <inge.diego94@gmail.com>
2025-06-16 13:36:22 -0500

    Nuevos archivos

---
+++ 07_condicionales/console_assert.js
@@ -0,0 +1,12 @@
+/*
+
+CONSOLE.ASSERT():
+
+Muestra un mensaje si la condición que le pasas es falsa. Se utiliza para hacer comprobaciones rápidas de condiciones en el código. 
+Si la condición es falsa, muestra el mensaje de error, de lo contrario no se muestra nada.
+
+*/ 
+
+let validacion = false;
+
+console.assert(validacion, "La validacion es falsa.");
---
+++ 07_condicionales/operador_ternario.js
@@ -0,0 +1,13 @@
+/*
+OPERADOR TERNARIO:
+
+Es una forma compacta de escribir una condición “if...else”. Se usa comúnmente cuando solo hay una condición 
+simple para evaluar y se desea devolver un valor diferente dependiendo si la condición se cumple o no.
+
+*/
+
+let edad = 18;
+
+let estado = (edad >= 18) ? 'Adulto' : 'Menor';
+
+console.log(estado);
