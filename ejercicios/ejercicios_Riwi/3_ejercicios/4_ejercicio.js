3c9f9f05d5c0aa6b50a7a95f617b1e7b18020caf
ingediego94 <inge.diego94@gmail.com>
2025-06-20 13:36:59 -0500

    Se agrega login

--- ejercicios/ejercicios_Riwi/3_ejercicios/4_ejercicio.js
+++ ejercicios/ejercicios_Riwi/3_ejercicios/4_ejercicio.js
@@ -2,6 +2,46 @@
 // Como administrador del sistema, quiero validar si un usuario tiene credenciales y rol de
 // superusuario para permitir o denegar el acceso a funcionalidades críticas.
 
-let usuario_1 = 'user';
-let usuario_2 = 'admin';
+const usuario_1 = 'admin';
+const password_1 = '201480';
 
+const usuario_2 = 'user';
+const password_2 = '45237';
+
+let acceso = false;
+let usario_count = 0;
+// declaring the variable
+// initializing the variable
+
+let attempts = 0;
+
+
+
+while(acceso == false){
+
+    let entrada1 = prompt("Ingrese su usuario: ");
+    let entrada2 = prompt("Ingrese su contraseña: ");
+
+    if (entrada1 === usuario_1 ){
+        if( entrada2 === password_1 && usario_count < 3){
+            alert(`Se te ha otorgado acceso al sistema como administración. You're superadmin. Llevas ${attempts}`);
+            acceso = true;
+            break;
+        }
+        else {
+            alert("you user is blok or the password and user is incorret ")
+            usario_count ++; 
+        }
+    } 
+    else if(entrada1 === usuario_2 && entrada2 === password_2 ){
+        alert(`Has ingresado al sistema como usuario sin permisos sin edición. Llevas ${attempts}`);
+        acceso = true;
+        break;
+    }
+    else {
+        acceso = false;
+        attempts ++;
+        alert(`Se ha presentado un error. Llevas ${attempts}`);
+    }
+
+}
